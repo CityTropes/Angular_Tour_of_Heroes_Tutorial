d0538efa33dad48379021feee2f988d27d73dda1
Jan De Pelsmacker <jan.depelsmacker@gmail.com>
2022-01-06 15:14:03 +0100

    2. Display a list

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const  HEROES: Hero[] = [
+  { id: 11, name: 'Homer Simpson'},
+  { id: 12, name: 'My Mommy'},
+  { id: 13, name: 'JanMan'},
+  { id: 14, name: 'Bob Ross'},
+  { id: 15, name: 'Steve Wozniak'},
+  { id: 16, name: 'Philo Farnsworth'},
+  { id: 17, name: 'Tim V.'},
+  { id: 18, name: 'Philip J. Fry'},
+  { id: 19, name: 'Donatello Turtle'},
+  { id: 20, name: 'Scrooge McDuck'},
+]
